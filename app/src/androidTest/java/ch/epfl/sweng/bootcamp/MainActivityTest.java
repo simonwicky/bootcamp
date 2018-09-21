909d7710ceb89baa96e47d096cc33e0dd58f68eb
Simon Wicky <simon.wicky@epfl.ch>
2018-09-21 11:29:06 +0200

    added travis.yml

--- app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -1,7 +1,5 @@
 package ch.epfl.sweng.bootcamp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,7 +12,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
